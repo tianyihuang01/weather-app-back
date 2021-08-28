f8ad7b2fc2977b8645678c009076649a4d5fe447
Tianyi Huang <h.tianyi@outlook.com>
2021-08-28 21:20:07 +1000

    add weather api

---
+++ src/controllers/weather.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+const logger = require('../utils/logger');
+
+const getOneCall = async (req, res) => {
+  const { lon, lat } = req.query;
+  const base = 'https://api.openweathermap.org/data/2.5/onecall?';
+  const units = 'metric';
+  const exclude = 'minutely,hourly,alerts';
+  const API = process.env.WEATHER_API_KEY;
+  const url = `${base}lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${API}&units=${units}`;
+
+  const { status, data } = await axios.get(url);
+  logger.debug(status);
+  return res.json(data);
+};
+
+module.exports = { getOneCall };
