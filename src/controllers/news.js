7e7dc18b0bf200e7354def97ee9e64933eb0ea3e
Tianyi Huang <h.tianyi@outlook.com>
2021-09-08 19:33:46 +1000

    remove unused controller function

--- src/controllers/news.js
+++ src/controllers/news.js
@@ -1,21 +1,6 @@
 const axios = require('axios');
 const logger = require('../utils/logger');
 
-const getTopHeadlines = async (req, res) => {
-  const { country, q, pageSize } = req.query;
-  const base = 'https://newsapi.org/v2/top-headlines?';
-
-  const config = {
-    headers: { Authorization: `Bearer ${process.env.NEWS_API_KEY}` },
-  };
-
-  const url = `${base}country=${country}&q=${q}&pageSize=${pageSize}`;
-
-  const { status, data } = await axios.get(url, config);
-  logger.debug(status);
-  return res.json(data);
-};
-
 const getEverything = async (req, res) => {
   const { q, pageSize } = req.query;
   const base = 'https://newsapi.org/v2/everything?';
@@ -31,4 +16,4 @@ const getEverything = async (req, res) => {
   return res.json(data);
 };
 
-module.exports = { getTopHeadlines, getEverything };
+module.exports = { getEverything };
