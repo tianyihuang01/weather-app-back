f7516ff74aca1e3d9ff67af83eddeb7634374ce7
Tianyi Huang <h.tianyi@outlook.com>
2021-09-08 19:35:27 +1000

    remove unused cities controller

--- src/controllers/cities.js
+++ src/controllers/cities.js
@@ -1,18 +1,5 @@
 const City = require('../models/city');
 
-const getAllCities = async (req, res) => {
-  const cities = await City.find().exec();
-  if (!cities) return res.sendStatus(404);
-  return res.json(cities);
-};
-
-const getCityByObjectId = async (req, res) => {
-  const { id } = req.params;
-  const city = await City.findById(id).exec();
-  if (!city) return res.sendStatus(404);
-  return res.json(city);
-};
-
 const getCityByCityId = async (req, res) => {
   const { id } = req.params;
   const city = await City.findOne({ id }).exec();
@@ -29,4 +16,4 @@ const getCityByField = async (req, res) => {
   return res.json(cities);
 };
 
-module.exports = { getAllCities, getCityByObjectId, getCityByCityId, getCityByField };
+module.exports = { getCityByCityId, getCityByField };
