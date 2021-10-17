58826c499d904d19c7fa26e7ebae2ef84a2a8ebf
Tianyi Huang <h.tianyi@outlook.com>
2021-10-17 16:06:31 +1100

    Update api call url

--- src/controllers/weather.js
+++ src/controllers/weather.js
@@ -5,7 +5,7 @@ const getOneCall = async (req, res) => {
   const { lon, lat } = req.query;
   const base = 'https://api.openweathermap.org/data/2.5/onecall?';
   const units = 'metric';
-  const exclude = 'minutely,hourly,alerts';
+  const exclude = 'minutely';
   const API = process.env.WEATHER_API_KEY;
   const url = `${base}lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${API}&units=${units}`;
 
